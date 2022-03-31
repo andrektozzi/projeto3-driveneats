93d39680b8d72c784403709198ff48bf68663104
Fulano <fulano@gmail.com>
2022-03-31 10:57:23 -0300

    segunda versao

--- scripts.js
+++ scripts.js
@@ -1,60 +1,90 @@
 let prato
+let bebida
+let sobremesa
+let valorPrato
+let valorBebida
+let valorSobremesa
 
-function escolherFrangoEsquerda() {
-  document.getElementById("frango-esquerda").style.borderColor = "green";
-  document.getElementById("frango-meio").style.borderColor = "white";
-  document.getElementById("frango-direita").style.borderColor = "white";
+function escolherFrango() {
+  document.getElementById("frango").style.borderColor = "green";
+  document.getElementById("carne").style.borderColor = "white";
+  document.getElementById("macarrao").style.borderColor = "white";
   prato = "Frango"
+  valorPrato = 14.90
 }
 
-function escolherFrangoMeio() {
-    document.getElementById("frango-esquerda").style.borderColor = "white";
-    document.getElementById("frango-meio").style.borderColor = "green";
-    document.getElementById("frango-direita").style.borderColor = "white";
+function escolherCarne() {
+    document.getElementById("frango").style.borderColor = "white";
+    document.getElementById("carne").style.borderColor = "green";
+    document.getElementById("macarrao").style.borderColor = "white";
+    prato = "Carne"
+    valorPrato = 24.90
   }
 
-  function escolherFrangoDireita() {
-    document.getElementById("frango-esquerda").style.borderColor = "white";
-    document.getElementById("frango-meio").style.borderColor = "white";
-    document.getElementById("frango-direita").style.borderColor = "green";
+  function escolherMacarrao() {
+    document.getElementById("frango").style.borderColor = "white";
+    document.getElementById("carne").style.borderColor = "white";
+    document.getElementById("macarrao").style.borderColor = "green";
+    prato = "Macarrao"
+    valorPrato = 18.90
   }
 
-  function escolherCocaEsquerda() {
-    document.getElementById("coca-esquerda").style.borderColor = "green";
-    document.getElementById("coca-meio").style.borderColor = "white";
-    document.getElementById("coca-direita").style.borderColor = "white";
+  function escolherCoca() {
+    document.getElementById("coca").style.borderColor = "green";
+    document.getElementById("suco").style.borderColor = "white";
+    document.getElementById("cha").style.borderColor = "white";
+    bebida = "Coca"
+    valorBebida = 4.90
   }
 
-  function escolherCocaMeio() {
-    document.getElementById("coca-esquerda").style.borderColor = "white";
-    document.getElementById("coca-meio").style.borderColor = "green";
-    document.getElementById("coca-direita").style.borderColor = "white";
+  function escolherSuco() {
+    document.getElementById("coca").style.borderColor = "white";
+    document.getElementById("suco").style.borderColor = "green";
+    document.getElementById("cha").style.borderColor = "white";
+    bebida = "Suco"
+    valorBebida = 6.90
   }
 
-  function escolherCocaDireita() {
-    document.getElementById("coca-esquerda").style.borderColor = "white";
-    document.getElementById("coca-meio").style.borderColor = "white";
-    document.getElementById("coca-direita").style.borderColor = "green";
+  function escolherCha() {
+    document.getElementById("coca").style.borderColor = "white";
+    document.getElementById("suco").style.borderColor = "white";
+    document.getElementById("cha").style.borderColor = "green";
+    bebida = "Cha"
+    valorBebida = 5.90
   }
 
-  function escolherPudimEsquerda() {
-    document.getElementById("pudim-esquerda").style.borderColor = "green";
-    document.getElementById("pudim-meio").style.borderColor = "white";
-    document.getElementById("pudim-direita").style.borderColor = "white";
+  function escolherPudim() {
+    document.getElementById("pudim").style.borderColor = "green";
+    document.getElementById("bolo").style.borderColor = "white";
+    document.getElementById("sorvete").style.borderColor = "white";
+    sobremesa = "Pudim"
+    valorSobremesa = 7.90
   }
 
-  function escolherPudimMeio() {
-    document.getElementById("pudim-esquerda").style.borderColor = "white";
-    document.getElementById("pudim-meio").style.borderColor = "green";
-    document.getElementById("pudim-direita").style.borderColor = "white";
+  function escolherBolo() {
+    document.getElementById("pudim").style.borderColor = "white";
+    document.getElementById("bolo").style.borderColor = "green";
+    document.getElementById("sorvete").style.borderColor = "white";
+    sobremesa = "Bolo"
+    valorSobremesa = 8.90
   }
 
-  function escolherPudimDireita() {
-    document.getElementById("pudim-esquerda").style.borderColor = "white";
-    document.getElementById("pudim-meio").style.borderColor = "white";
-    document.getElementById("pudim-direita").style.borderColor = "green";
+  function escolherSorvete() {
+    document.getElementById("pudim").style.borderColor = "white";
+    document.getElementById("bolo").style.borderColor = "white";
+    document.getElementById("sorvete").style.borderColor = "green";
+    sobremesa = "Sorvete"
+    valorSobremesa = 5.90
   }
 
   function finalizarPedido() {
-      alert(prato)
+      let mensagem;
+
+      mensagem = "Olá, gostaria de fazer o pedido: " +
+    "\n- Prato: " + prato +
+    "\n- Bebida: " + bebida +
+    "\n- Sobremesa: " + sobremesa + 
+    "\nTotal: R$" + Number(valorPrato + valorBebida + valorSobremesa).toFixed(2)
+
+    window.open("https://wa.me/+5519992019243?text=" + mensagem)
   }
